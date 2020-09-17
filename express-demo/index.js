e70d41fa3e0b91e65393519d01c873c25267befb
Ilya Androsov <ilya_androsov@icloud.com>
2020-09-18 00:52:14 +0300

    feat: add course selection by id

--- express-demo/index.js
+++ express-demo/index.js
@@ -3,12 +3,35 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const courses = [
+  {
+    id: 1,
+    name: "course 1",
+  },
+
+  {
+    id: 2,
+    name: "course 2",
+  },
+  {
+    id: 3,
+    name: "course 3",
+  },
+];
+
 app.get("/", (req, res) => {
   res.send("Hello World!!!");
 });
 
 app.get("/api/courses", (req, res) => {
-  res.send([1, 2, 3]);
+  res.send(courses);
+});
+
+app.get("/api/courses/:id", (req, res) => {
+  const course = courses.find((item) => item.id === parseInt(req.params.id));
+
+  if (!course) res.status(404).send("No course with the given id");
+  res.send(course);
 });
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
