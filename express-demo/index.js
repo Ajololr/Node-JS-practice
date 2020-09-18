af62aa7c56b6ea027613e72cf87af25f7e469574
Ilya Androsov <ilya_androsov@icloud.com>
2020-09-18 12:23:48 +0300

    feat: add name validation

--- express-demo/index.js
+++ express-demo/index.js
@@ -37,6 +37,9 @@ app.get("/api/courses/:id", (req, res) => {
 });
 
 app.post("/api/courses/", (req, res) => {
+  if (!req.body.name || req.body.name.length < 3)
+    res.status(400).send("Name is required and must be at least 3 characters");
+
   const course = {
     id: courses.length + 1,
     name: req.body.name,
