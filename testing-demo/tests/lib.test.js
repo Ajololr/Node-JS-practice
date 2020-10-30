8891ab8339763a0ce1cf0d218ef34e5dc6ed287a
Ilya Androsov <ilya_androsov@icloud.com>
2020-10-30 03:16:13 +0300

    refactor: add describe

--- testing-demo/tests/lib.test.js
+++ testing-demo/tests/lib.test.js
@@ -1,16 +1,18 @@
 const lib = require("../lib");
 
-test("absolute should return positive number if input is positive", () => {
-  const res = lib.absolute(1);
-  expect(res).toBe(1);
-});
+describe("absolute", () => {
+  it("should return positive number if input is positive", () => {
+    const res = lib.absolute(1);
+    expect(res).toBe(1);
+  });
 
-test("absolute should return positive number if input is negative", () => {
-  const res = lib.absolute(-1);
-  expect(res).toBe(1);
-});
+  it("should return positive number if input is negative", () => {
+    const res = lib.absolute(-1);
+    expect(res).toBe(1);
+  });
 
-test("absolute should return 0 number if input is 0", () => {
-  const res = lib.absolute(0);
-  expect(res).toBe(0);
+  it("should return 0 number if input is 0", () => {
+    const res = lib.absolute(0);
+    expect(res).toBe(0);
+  });
 });
